6b394c6524bbe0d5860fdf892ced96e0e00cdea3
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-01-29 16:17:45 -0800

    Finish phase 1 of 99 cats

--- ninety_nine__cats/app/controllers/cats_controller.rb
+++ ninety_nine__cats/app/controllers/cats_controller.rb
@@ -14,7 +14,37 @@ class CatsController < ApplicationController
         end
     end
 
-    
+    def new
+        @cat = Cat.new
+        render :new
+    end
 
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            render :show
+        else
+            redirect_to cats_url(@cat)
+        end
+    end
 
+    def edit
+        @cat = Cat.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+    @cat = Cat.find(params[:id])
+    if @cat.update_attributes(cat_params)
+        redirect_to cat_url(@cat)
+    else 
+        render :edit
+    end
+end
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
+    end
 end
